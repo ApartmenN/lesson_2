b3bb3e08716b4dba952fd6ac7ebd1a34fdf089ee
ApartmanN <65243790+ApartmenN@users.noreply.github.com>
2024-01-29 01:03:33 +0300

    Update bornyearforewer.py

--- bornyearforewer.py
+++ bornyearforewer.py
@@ -1,1 +1,8 @@
-
+# 6 июня 1799
+while True:
+    years = str(input('Введите год рождения Пушкина: '))
+    if years == '1799':
+        print('Верно')
+        break
+    else:
+        print('Неверно')
